aa12a5bd6befb170e293b8d0dac8c3d0e297f962
kajapm <112996805+kajapm@users.noreply.github.com>
2022-11-16 16:21:50 +0530

    Add files via upload

---
+++ Thirukkural-Viewer/src/com/kaja/thirukkuralviewer/view/ThirukkuralViewer.java
@@ -0,0 +1,8 @@
+package com.kaja.thirukkuralviewer.view;
+
+public class ThirukkuralViewer {
+    public static void main(String args[]) {
+        View view = new View();
+        view.mainMenu();
+    }
+}
