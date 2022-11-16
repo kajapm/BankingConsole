a98310859a2f8484282a193f3c58f4ee643ed273
kajapm <112996805+kajapm@users.noreply.github.com>
2022-11-16 12:31:42 +0530

    Add files via upload

---
+++ Flight-ticket-booking/src/com/kaja/flightticketbooking/model/Passenger.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Passenger {
+	private String name;
+	private int age;
+	private String gender;
+	private String passportId;
+	private String contactNumber;
+	private String emergencyContact;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPassportId() {
+		return passportId;
+	}
+
+	public void setPassportId(String passportId) {
+		this.passportId = passportId;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getEmergencyContact() {
+		return emergencyContact;
+	}
+
+	public void setEmergencyContact(String emergencyContact) {
+		this.emergencyContact = emergencyContact;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
