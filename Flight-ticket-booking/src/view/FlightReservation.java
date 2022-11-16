a865a652e3dd8023351f6fef65cedc737b19e9f2
kajapm <112996805+kajapm@users.noreply.github.com>
2022-11-16 12:29:16 +0530

    Delete Flight-ticket-booking/src directory

--- Flight-ticket-booking/src/view/FlightReservation.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-public class FlightReservation {
-	public static void main(String[] args) {
-		FlightReservationView flightReservationView = new FlightReservationView();
-		flightReservationView.showWelcomeMessage();
-		flightReservationView.showMainMenu();
-	}
-}
